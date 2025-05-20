602524d832af14196cb83ba5cf8c6f852aed3201
Delyan Angelov <delian66@gmail.com>
2025-05-20 13:20:14 +0300

    pickup more changes from the original models_first_person_maze.c

--- raylib_fps.v
+++ raylib_fps.v
@@ -4,9 +4,7 @@ import raylib as r
 
 fn main() {
 	r.init_window(800, 450, 'raylib [models] example - first person maze')
-	r.set_target_fps(60)
 
-	map_position := r.Vector3{-16.0, 0.0, -8.0}
 	mut camera := r.Camera{
 		position:   r.Vector3{0.2, 0.4, 0.2}
 		target:     r.Vector3{0.185, 0.4, 0.0}
@@ -14,10 +12,10 @@ fn main() {
 		fovy:       45.0
 		projection: 0 // CAMERA_PERSPECTIVE
 	}
-	r.disable_cursor() // Limit cursor to relative movement inside the window
 
 	immap := r.load_image(os.resource_abs_path('cubicmap.png'))
 	cubicmap := r.load_texture_from_image(immap)
+
 	mesh := r.gen_mesh_cubicmap(immap, r.Vector3{1.0, 1.0, 1.0})
 	mut model := r.load_model_from_mesh(mesh)
 
@@ -29,9 +27,12 @@ fn main() {
 	map_pixels := r.load_image_colors(immap)
 	r.unload_image(immap)
 
+	map_position := r.Vector3{-16.0, 0.0, -8.0}
+	r.disable_cursor() // Limit cursor to relative movement inside the window
+	r.set_target_fps(60)
 	for !r.window_should_close() { // Detect window close button or ESC key
 		old_cam_pos := camera.position
-		r.update_camera(&camera, C.CAMERA_THIRD_PERSON)
+		r.update_camera(&camera, C.CAMERA_FIRST_PERSON)
 
 		// Check player collision (we simplify to 2D collision detection)
 		player_pos := r.Vector2{camera.position.x, camera.position.z}
@@ -41,17 +42,8 @@ fn main() {
 		mut player_cell_y := int(player_pos.y - map_position.z + 0.5)
 
 		// Out-of-limits security check
-		if player_cell_x < 0 {
-			player_cell_x = 0
-		} else if player_cell_x >= cubicmap.width {
-			player_cell_x = cubicmap.width - 1
-		}
-
-		if player_cell_y < 0 {
-			player_cell_y = 0
-		} else if player_cell_y >= cubicmap.height {
-			player_cell_y = cubicmap.height - 1
-		}
+		player_cell_x = int_max(0, int_min(cubicmap.width - 1, player_cell_x))
+		player_cell_y = int_max(0, int_min(cubicmap.height - 1, player_cell_y))
 
 		// Check map collisions using image data and player position
 		// TODO: Improvement: Just check player surrounding cells for collision
@@ -60,7 +52,7 @@ fn main() {
 				// Collision: white pixel, only check R channel
 				if unsafe { map_pixels[y * cubicmap.width + x].r == 255 } {
 					if r.check_collision_circle_rec(player_pos, player_radius, r.Rectangle{
-						map_position.x - 0.5 + f32(x) * 1.0, map_position.z - 0.5 + f32(y) * 1.0, 1.0, 1.0})
+						map_position.x - 0.5 + f32(x), map_position.z - 0.5 + f32(y), 1.0, 1.0})
 					{
 						// Collision detected, reset camera position
 						camera.position = old_cam_pos
@@ -73,7 +65,7 @@ fn main() {
 		{
 			r.clear_background(r.raywhite)
 
-			r.begin_mode_3d(r.Camera3D(camera))
+			r.begin_mode_3d(camera)
 			r.draw_model(model, map_position, 1.0, r.white) // Draw maze map
 			r.end_mode_3d()
 
